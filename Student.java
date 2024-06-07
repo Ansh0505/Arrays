26f8d9a16f808400c96393a5065e3cd26b8f089f
Ansh0505 <110764319+Ansh0505@users.noreply.github.com>
2024-06-07 12:04:16 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,54 @@
+public class Student
+{
+  private int sid;
+  private String sname;
+  public Student(int sid,String sname)
+  {
+	super();
+	this.sid=sid;
+	this.sname=sname;
+  }
+   public int getSid()
+   {
+	return sid;
+   }
+    public void setSid(int sid)
+    {
+	 this.sid=sid;
+    }
+     public String getSname()
+     {
+	  return sname;	
+     } 
+      public void setSname(String sname)
+      {
+	   this.sname=sname;
+      }
+}     
+ class Department
+ {
+   private String dname;
+   private Student student;
+   public Department(String dname,Student student)
+   {
+    super();
+    this.dname = dname;
+    this.student = student;
+   }
+      void show()
+      {
+    	System.out.println("Department Name ="+dname+"/n"+"Student="+student.getSid()+""+student.getSname());
+      }
+    }   
+     class Aggregation
+     {
+	  public static void main(String[] args) 
+	  {
+	   Student s1=new Student(121, "Sneha");
+	   Student s2=new Student(122, "Ansh");
+	   Department d=new Department("Coputer Science", s1);
+	   Department d1=new Department("Polytechnic",s2);
+	   d.show();
+	   d1.show();
+	  }
+}
